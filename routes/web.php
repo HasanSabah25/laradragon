7e65cc28f7a9a06151e2c0befa8b532425218bea
HasanSabah25 <hasansabah786@gmail.com>
2026-03-22 22:41:51 +0300

    test supervisor fix

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test', function () {
+Route::get('/test-job', function () {
     UpdateProductQtyJob::dispatch();
     return response()->json(['success']);
 });
