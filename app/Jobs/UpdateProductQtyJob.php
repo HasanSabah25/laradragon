e01a41dd040eb95963d5e05d90df2c97c215a398
HasanSabah25 <92628172+HasanSabah25@users.noreply.github.com>
2026-03-28 14:44:03 +0300

    Staging (#3)
    * staging
    
    * change
    
    * change
    
    * change
    
    * change
    
    * change
    
    * change
    
    * go back to basic laravel
    
    * add ci/cd
    
    * test ci/cd
    
    * test ci/cd
    
    * test supervisor
    
    * test supervisor fix
    
    * test ci/cd add route clear
    
    * add redis testing

---
+++ app/Jobs/UpdateProductQtyJob.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Product;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class UpdateProductQtyJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        Product::query()->update(['price' => random_int(20000, 100000)]);
+    }
+}
